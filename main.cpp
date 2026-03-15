961a41f0b008c071d22890cb4a676005954163ee
vickydel <120592899+vickydel@users.noreply.github.com>
2026-03-14 17:14:44 -0700

    Uploaded the main.cpp file

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/********************************************************************************
+Author(s) Group 3: Gurleen Kaur, Tran Dat Le, Vanessa Salinas, Vickydel Gutierrez
+Course: CSCI 115 Project
+Assignment: Sorting Algorithms
+
+*******************************************************************************/
+#include <iostream>
+#include <ctime>
+#include "SelectionSort.cpp"
+//------------------------------------------------------TO-DO-------------------------------------------------------------------
+/*Week 7	Mar 2–Mar 8	
+Implement 3 simple sorts (Insertion/Selection/Bubble)
+Define input generators (best/avg/worst)
+Decide input sizes (n values) + repetitions
+Code runs on small n; input generator produces intended cases
+Week 8	Mar 9–Mar 15	
+Implement Merge + Heap
+Implement Quicksort (best version / randomized or median-of-3)
+Validate correctness (unit tests)
+All implemented sorts pass correctness tests on random + edge cases*/
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+using namespace std;
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------INT MAIN----------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+int main() {
+    
+    
+    cout << "Choose one of the following sorting algorithms: \n";
+    cout << "[1]: Bubble Sort, [2]: Selection Sort, [3]: Insertion Sort, [4]: Merge Sort, [5]: Quick Sort, [6]: Heap Sort, [7]: Counting Sort, [8]: Radix Sort\n";
+
+    
+    return 0;
+}
